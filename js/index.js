2687c88ce9a5471895fbc53bf3d61d713bb4daf5
Hector Serrano <hserrano1030@gmail.com>
2022-09-27 17:59:36 -0500

    Added a form and basic script for it

--- js/index.js
+++ js/index.js
@@ -14,3 +14,34 @@ for (let i = 0; i < skills.length; i++) {
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
+
+const messageForm = document.querySelector('[name = leave_message] ');
+messageForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const message = event.target.message.value;
+    console.log(`name: ${name}, email: ${email}, message: ${message}`);
+    
+    const messageSection = document.querySelector('#messages');
+
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = `<a href="mailto:${email}">${name}</a><span> wrote: ${message} </span>`;
+
+    const removeButton = document.createElement('button');
+    removeButton.setAttribute('type', 'button');
+    removeButton.innerText = 'Remove';
+    removeButton.addEventListener('click', onRemoveButtonClick);
+
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+    messageForm.reset();
+});
+
+function onRemoveButtonClick(event) {
+    const entry = event.target.parentNode;
+    entry.remove();
+
+}
